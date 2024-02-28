c478157d1659babdd5a7533036512714de750d46
Bektur1902 <bektur1902@gmail.com>
2024-02-28 23:37:10 +0600

    Добавлено задание по теме логические операторы

---
+++ lesson32/main.js
@@ -0,0 +1,28 @@
+// let age = prompt('Enter age')
+
+// if (age == 14 && 90) {
+//     console.log('true');
+// }else{
+//     console.log('false');
+// }
+
+// let a = '', b = 7, c = 1, d = 2;
+
+// alert(a || b && b || d)
+
+// let age = prompt('Enter your age:')
+// let hasLicince = prompt('Do you have a licence?')
+
+// if (age && hasLicince) {
+//     console.log(true);
+// }else {
+//     console.log(false);
+// }
+
+// let age = 17
+
+// if (age < 18 || age > 65) {
+//     console.log(true);
+// }else {
+//     console.log(false);
+// }
