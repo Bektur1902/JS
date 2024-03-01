152eeb4fca7c22547af6bc04aceaf3ebfb942859
Bektur1902 <bektur1902@gmail.com>
2024-03-01 23:27:31 +0600

    Дз по массивам

---
+++ lesson33/main.js
@@ -0,0 +1,43 @@
+// let a = Number(prompt('Enter num: '))
+
+// if (a % 2 == 0 && !0) {
+//     console.log(true);
+// }
+
+// let a = Number(prompt('Enter num: '))
+
+// if (a >= 10 && a <= 100){
+//     console.log(true);
+// }
+
+// let a = Number(prompt('Enter num: '))
+
+// if (a % 2 == 0 && a > 10) {
+//     console.log('Число является четным и больше 10');
+// } else if(a % 2 != 0 && a > 10) {
+//     console.log('Число является нечетным и больше 10');
+// }
+
+// let a = Number(prompt('Enter a'))
+// let b = Number(prompt('Enter b'))
+
+// if (a % 2 == 0 && b % 2 == 0) {
+//     console.log(a * b);
+// } else if(a % 2 != 0 && b % 2 != 0){
+//     console.log(a + b);
+// }else {
+//     console.log(a , b);
+// }
+
+// alert( null || 2 || undefined ); == 2
+// alert( null || 2 && 3 || 4 ); == 3
+// alert( !null || 2 ) == true
+// alert( !undefined || 0 ) == true
+// let x
+// alert(x ?? 5); == 5
+
+
+// let a = [1, 2, 3, 4]
+// a[2]=5
+// a.push(7)
+// console.log(a, a.length);
