0fa46adea6291585f247a8aebb25115c1a1a2b0c
Bektur1902 <bektur1902@gmail.com>
2024-03-07 00:00:02 +0600

    Сложные методы массивов

---
+++ lesson35/main.js
@@ -0,0 +1,25 @@
+// let a = [1, 2, 3, 4]
+// console.log(a.join('-'));
+
+// let user = prompt('Enter something: ');
+// console.log(user.split(' '));
+
+// let a = '2024.03.06';
+// console.log(a.split('.').reverse().join('-'));
+
+let usersRecords = [{ name: 'John', id: 123, marks: 98, pole: "mentor", age: 35 },
+{ name: 'Baba', id: 101, marks: 23, pole: "student", age: 12 },
+{ name: 'yaga', id: 200, marks: 45, pole: "student", age: 18 },
+{ name: 'Wick', id: 115, marks: 75, pole: "mentor", age: 24 }]
+
+// const result = usersRecords.map((item) => item.name)
+// result.forEach((item) => console.log(item))
+
+// const result = usersRecords.findIndex((item) => item.age === 18)
+// console.log(result);
+
+// const result = usersRecords.map((item) => item.name);
+// console.log(result.join(' '));
+
+// const result = usersRecords.filter((item) => item.marks > 44).map((item) => item.marks + 20)
+// console.log(result);
