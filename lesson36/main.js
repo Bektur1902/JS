2f9db314da75b4a0ad3fa81f7a9f904f9ebcd4b2
Bektur1902 <bektur1902@gmail.com>
2024-03-15 17:57:14 +0600

    Добавлена дз по циклам (for, while, do while)

---
+++ lesson36/main.js
@@ -0,0 +1,64 @@
+// for (let i = 0; i < 3; i++) {
+//     alert( `number ${i}!` );
+//     }
+
+// let i = 0;
+// while (i<3) {
+//     i += 1
+//     alert(`number${i}!`)
+// }
+
+let arr = [1, 3, 4, 6, 9, 11, 24, 17];
+
+// for (let i = 0; i < arr.length; i++) {
+//     let result = true;
+
+//     if (arr[i] <= 1) {
+//         result = false;
+//     } else {
+//         for (let j = 2; j <= Math.sqrt(arr[i]); j++) {
+//             if (arr[i] % j === 0) {
+//                 result = false;
+//                 break
+//             }
+//         }
+//     }
+//     if(result){
+//         console.log(`${arr[i]}`);
+//     }
+// }
+
+// let revesedArr = []
+
+// for (let i = arr.length - 1; i >=0; i--){
+//     revesedArr.push(arr[i]);
+// }
+// console.log(revesedArr);
+
+// let i =3;
+
+// while(i<=7){
+//     console.log(i);
+//     i += 1;
+// }
+
+// let firstNum = null;
+
+// for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] % 2 === 0) {
+//         firstNum = arr[i];
+//         break
+//     }
+// }
+
+// if (firstNum != null) {
+//     console.log(firstNum);
+// } else {
+//     console.log('Четного числа нет');
+// }
+
+// for (let i = 1; i <= 10; i++){
+//     if(i != 5){
+//         console.log(i);
+//     }
+// }
