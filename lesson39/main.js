46db01d8af8cfcbed2bb3aa85cbccb1898502e10
Bektur1902 <bektur1902@gmail.com>
2024-04-01 19:03:22 +0600

    Add function1 lesson

---
+++ lesson39/main.js
@@ -0,0 +1,45 @@
+// Скидываем репозиторием!
+// Задание 1
+// Напишите функцию, которая принимает в себя строку(параметр) и убирает из неё все пробелы. (возвращает строку без пробелов)
+// "    hello my friends   " --> "hellomyfriends"
+
+// Задание 2
+// Напишите функцию, которая принимает массив и возвращает его последний элемент
+
+// Задание 3
+// Напишите функцию, которая принимает два массива, соединяет их и возвращает
+
+// Задание 4
+// Напишите функцию, которая принимает в себя строку и делает первую букву заглавной
+// Например: 'привет' --> 'Привет'
+
+
+// function withoutSpace(str) {
+//     return str.replace(/\s/g, '');
+// }
+
+// console.log(withoutSpace("    hello my friends   "));
+
+// function someArray(array) {
+//     return array.pop();
+// }
+
+// let arr = [1,2,3,4,5,6];
+
+// console.log(someArray(arr));
+
+// function concatArray(x, y){
+//     return x.concat(y);
+// }
+
+// let x = [12];
+// let y = [12341];
+
+// console.log(concatArray(x,y));
+
+
+// function firstLetter(value) {
+//     return value.charAt(0).toUpperCase() + value.slice(1);
+// }
+
+// console.log(firstLetter('привет'));
